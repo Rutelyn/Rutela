ced6cd38c4fb2a1843ec983377a3057fbd574951
Rutela <ruth.agbozo.93@gmail.com>
2024-06-11 09:27:13 +0000

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+
+const btn = document.getElementById('btn-btn')
+btn.onclick = function () {
+    alert('Hair consultancy available now!')
+    
+}
+const btn2 = document.getElementById('more')
+btn2.onclick = function () {
+    alert('Style up')
+}
